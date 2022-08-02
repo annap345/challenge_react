77c647f5a24fd7eff58623a6bbb92ee986aad79c
annap345 <107302838+annap345@users.noreply.github.com>
2022-08-02 21:19:30 +0300

    moved files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <body>
+      <h1 class="heading">Weather</h1>
+      <div class="app">
+        <div class="container">
+          <p class="input-search" id="search">
+            <form id="search-form">
+              <input type="text" placeholder="Enter a city" id="search-input" />
+              <input type="submit" value="search" class="submit" />
+            </form>
+          </p>
+          <div class="date">
+            <p>2 June, Thursday</p>
+          </div>
+          <div class="row align-items-start main-forecast">
+            <div class="col-1 city">
+              <h2 id="main-city">Kyiv</h2>
+              <div id="main-weather">
+                <i class="fa-solid fa-sun sun"></i>
+              </div>
+            </div>
+            <div class="col-1 main">
+              <h2 class="degree">
+                <span id="main-degree">16 </span>
+                <span id="celcius">°C </span> /<span id="fahrenheit">°F</span>
+              </h2>
+              <p class="small-degree" id="small-degrees">
+                <span id="degree-max">20</span>°C ~{" "}
+                <span id="degree-min">13</span>°C
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="more">
+          <p class="weather" id="visibility">
+            sunny
+          </p>
+          <p class="wind" id="wind-speed">
+            wind: 20 km/h
+          </p>
+        </div>
+        <div class="forecast">
+          <div class="row align-items-center">
+            <div class="col">
+              <h3 class="day" id="day-one">
+                W
+              </h3>
+              <div>
+                <p class="degree" id="first-day">
+                  19°
+                </p>
+              </div>
+              <span id="weather-one">
+                <i class="fa-solid fa-sun sun"></i>
+              </span>
+            </div>
+            <div class="col">
+              <h3 class="day" id="day-two">
+                Th
+              </h3>
+              <div>
+                <p class="degree" id="second-day">
+                  14°
+                </p>
+              </div>
+              <span id="weather-two">
+                <i class="fa-solid fa-cloud-sun cloud"></i>
+              </span>
+            </div>
+            <div class="col">
+              <h3 class="day" id="day-three">
+                F
+              </h3>
+              <div>
+                <p class="degree" id="third-day">
+                  17°
+                </p>
+              </div>
+              <span id="weather-three">
+                <i class="fa-solid fa-cloud-rain rain"></i>
+              </span>
+            </div>
+            <div class="col">
+              <h3 class="day" id="day-four">
+                St
+              </h3>
+              <div>
+                <p class="degree" id="fourth-day">
+                  23°
+                </p>
+              </div>
+              <span id="weather-four">
+                <i class="fa-solid fa-cloud-rain rain"></i>
+              </span>
+            </div>
+            <div class="col">
+              <h3 class="day" id="day-five">
+                S
+              </h3>
+              <div>
+                <p class="degree" id="fifth-day">
+                  18°
+                </p>
+              </div>
+              <span id="weather-five">
+                <i class="fa-solid fa-cloud-sun cloud"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="credit">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          coded by Anna Popovych <br />
+          <a href="https://github.com/annap345/weather-app" target="_blank">
+            Code on GitHub
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      <script src="src/script.js"></script>
+    </body>
   );
 }
 
