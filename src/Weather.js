4ff638046cd94efa3755b122b89315ebacd62f97
annap345 <107302838+annap345@users.noreply.github.com>
2022-08-08 22:46:34 +0200

    renamed to className

--- src/Weather.js
+++ src/Weather.js
@@ -5,114 +5,113 @@ import "bootstrap";
 export default function Weather() {
   return (
     <body>
-      <h1 class="heading">Weather</h1>
-      <div class="app">
-        <div class="container">
-          <p class="input-search" id="search">
+      <h1 className="heading">Weather</h1>
+      <div className="app">
+        <div className="container">
+          <p className="input-search" id="search">
             <form id="search-form">
               <input type="text" placeholder="Enter a city" id="search-input" />
-              <input type="submit" value="search" class="submit" />
+              <input type="submit" value="search" className="submit" />
             </form>
           </p>
-          <div class="row align-items-start main-forecast">
-            <div class="col-1 city">
+          <div className="row align-items-start main-forecast">
+            <div className="col-1 city">
               <h2 id="main-city">Kyiv</h2>
               <div id="main-weather">
-                <i class="fa-solid fa-sun sun"></i>
+                <i className="fa-solid fa-sun sun"></i>
               </div>
             </div>
-            <div class="col-1 main">
-              <h2 class="degree">
+            <div className="col-1 main">
+              <h2 className="degree">
                 <span id="main-degree">16 </span>
                 <span id="celcius">°C </span> /<span id="fahrenheit">°F</span>
               </h2>
-              <p class="small-degree" id="small-degrees">
+              <p className="small-degree" id="small-degrees">
                 <span id="degree-max">20</span>°C ~{" "}
                 <span id="degree-min">13</span>°C
               </p>
             </div>
           </div>
         </div>
-        <div class="more">
-          <p class="weather" id="visibility">
+        <div className="more">
+          <p className="weather" id="visibility">
             sunny
           </p>
-          <p class="wind" id="wind-speed">
+          <p className="wind" id="wind-speed">
             wind: 20 km/h
           </p>
         </div>
-        <div class="forecast">
-          <div class="row align-items-center">
-            <div class="col">
-              <h3 class="day" id="day-one">
+        <div className="forecast">
+          <div className="row align-items-center">
+            <div className="col">
+              <h3 className="day" id="day-one">
                 W
               </h3>
               <div>
-                <p class="degree" id="first-day">
+                <p className="degree" id="first-day">
                   19°
                 </p>
               </div>
               <span id="weather-one">
-                <i class="fa-solid fa-sun sun"></i>
+                <i className="fa-solid fa-sun sun"></i>
               </span>
             </div>
-            <div class="col">
-              <h3 class="day" id="day-two">
+            <div className="col">
+              <h3 className="day" id="day-two">
                 Th
               </h3>
               <div>
-                <p class="degree" id="second-day">
+                <p className="degree" id="second-day">
                   14°
                 </p>
               </div>
               <span id="weather-two">
-                <i class="fa-solid fa-cloud-sun cloud"></i>
+                <i className="fa-solid fa-cloud-sun cloud"></i>
               </span>
             </div>
-            <div class="col">
-              <h3 class="day" id="day-three">
+            <div className="col">
+              <h3 className="day" id="day-three">
                 F
               </h3>
               <div>
-                <p class="degree" id="third-day">
+                <p className="degree" id="third-day">
                   17°
                 </p>
               </div>
               <span id="weather-three">
-                <i class="fa-solid fa-cloud-rain rain"></i>
+                <i className="fa-solid fa-cloud-rain rain"></i>
               </span>
             </div>
-            <div class="col">
-              <h3 class="day" id="day-four">
+            <div className="col">
+              <h3 className="day" id="day-four">
                 St
               </h3>
               <div>
-                <p class="degree" id="fourth-day">
+                <p className="degree" id="fourth-day">
                   23°
                 </p>
               </div>
               <span id="weather-four">
-                <i class="fa-solid fa-cloud-rain rain"></i>
+                <i className="fa-solid fa-cloud-rain rain"></i>
               </span>
             </div>
-            <div class="col">
-              <h3 class="day" id="day-five">
+            <div className="col">
+              <h3 className="day" id="day-five">
                 S
               </h3>
               <div>
-                <p class="degree" id="fifth-day">
+                <p className="degree" id="fifth-day">
                   18°
                 </p>
               </div>
               <span id="weather-five">
-                <i class="fa-solid fa-cloud-sun cloud"></i>
+                <i className="fa-solid fa-cloud-sun cloud"></i>
               </span>
             </div>
           </div>
         </div>
       </div>
-
-      <div class="credit">
+      <div className="credit">
         <p>
           coded by Anna Popovych <br />
           <a
